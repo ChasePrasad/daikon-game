4663c26a64d4875a111a4572fe9758cc56a562a7
Chase Prasad <146298176+ChasePrasad@users.noreply.github.com>
2024-04-22 21:40:28 -0400

    Add files via upload

---
+++ vegetables.js
@@ -0,0 +1,52 @@
+export const VEGETABLES = [
+    {
+      label: "01_leek",
+      radius: 50 / 2,
+      color: "#F20306",
+    },
+    {
+      label: "02_carrot",
+      radius: 67 / 2,
+      color: "#FF624C",
+    },
+    {
+      label: "03_corn",
+      radius: 84 / 2,
+      color: "#A969FF",
+    },
+    {
+      label: "04_eggplant",
+      radius: 101 / 2,
+      color: "#FFAF02",
+    },
+    {
+      label: "05_broccoli",
+      radius: 118 / 2,
+      color: "#FC8611",
+    },
+    {
+      label: "06_potato",
+      radius: 135 / 2,
+      color: "#F41615",
+    },
+    {
+      label: "07_bell-pepper",
+      radius: 152 / 2,
+      color: "#FDF176",
+    },
+    {
+      label: "08_cauliflower",
+      radius: 169 / 2,
+      color: "#FEB6AC",
+    },
+    {
+      label: "09_onion",
+      radius: 186 / 2,
+      color: "#F7E608",
+    },
+    {
+      label: "10_radish",
+      radius: 203 / 2,
+      color: "#89CE13",
+    }
+  ];
